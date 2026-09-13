959ffeea52ff5422913d12c0660bc6dd5c8876c4
kristen-stuart <kl28stua@siena.edu>
2026-09-13 13:41:38 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,13 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Kristen Stuart
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("My favorite dish from my favorite restaurant, Hogan's diner, \nlocated in my hometown of Garnerville, NY, is called The Flag.");
     }
 }
